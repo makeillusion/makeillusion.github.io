5f71e55a37374a38a8c7dbd874f172a8bf501bdf
fedorguryanov <makeillusion4me@gmail.com>
2019-01-15 23:50:05 +0300

    Mobile

--- main.js
+++ main.js
@@ -77,9 +77,18 @@ $("#result-close").off().on('click', function () {
 });
 
 $("#result-save").off().on('click', function () {
-    canvas.toBlob(function (blob) {
-        saveAs(blob, 'illusion.png');
-    });
+    //canvas.toBlob(function (blob) {
+    //    saveAs(blob, 'illusion.png');
+    //});
+    var dt = canvas.toDataURL('image/png');
+    /* Change MIME type to trick the browser to downlaod the file instead of displaying it */
+    dt = dt.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
+
+    /* In addition to <a>'s "download" attribute, you can define HTTP-style headers */
+    dt = dt.replace(/^data:application\/octet-stream/, 'data:application/octet-stream;headers=Content-Disposition%3A%20attachment%3B%20filename=illusion.png');
+
+    window.open(dt);
+    //window.href = dt;
 });
 
 function previewFile(file) {
