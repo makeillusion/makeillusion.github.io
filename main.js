af886b9d833e879b4fe020fa42c4edc2caa19413
fedorguryanov <makeillusion4me@gmail.com>
2019-01-16 03:19:28 +0300

    Effects

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ function highlight(e) {
 }
 
 function unhighlight(e) {
-    dropArea.classList.remove('active');
+    dropArea.classList.remove('highlight');
 }
 
 function handleDrop(e) {
@@ -58,7 +58,7 @@ function processIII(idata, width, height) {
             base = residue > (step / 2 - 1) ? 230 : 25;
 
             if ((residue === 0) || (residue === (step - 1)) || (residue === (step / 2 - 1)) || (residue === (step / 2))) {
-                base = 128;
+            //    base = 128;
             }
             update = base + (128 + intense) / scale;
             idata.data[ind] = update;
@@ -68,6 +68,61 @@ function processIII(idata, width, height) {
 
     return idata;
 }
+
+function process___(idata, width, height) {
+    var ind, residue, intense, update, base;
+    var step = 16;
+    var scale = 16;
+    for (var y = 0; y < height; y++)
+        for (var x = 0; x < width; x++) {
+            ind = (y * width + x) * 4;
+            intense = (idata.data[ind] + idata.data[ind + 1] + idata.data[ind + 2]) / 3;
+            residue = (y + 4) % step;
+            base = residue > (step / 2 - 1) ? 230 : 25;
+
+            if ((residue === 0) || (residue === (step - 1)) || (residue === (step / 2 - 1)) || (residue === (step / 2))) {
+            //    base = 128;
+            }
+            update = base + (128 + intense) / scale;
+            idata.data[ind] = update;
+            idata.data[ind + 1] = update;
+            idata.data[ind + 2] = update;
+        }
+
+    return idata;
+}
+
+function processZZZ(idata, width, height) {
+    var ind, residue, intense, update, base;
+    var step = 16;
+    var scale = 16;
+    var wise = 64;
+    for (var y = 0; y < height; y++)
+        for (var x = 0; x < width; x++) {
+            ind = (y * width + x) * 4;
+            intense = (idata.data[ind] + idata.data[ind + 1] + idata.data[ind + 2]) / 3;
+
+            if (y % wise < (wise / 2)) {
+                residue = step - (width + y - x) % step;
+            } else {
+                //residue = (y - x) % step;
+                residue = (y + x) % step;
+            }
+
+            base = residue > (step / 2 - 1) ? 230 : 25;
+
+
+            update = base + (128 + intense) / scale;
+            idata.data[ind] = update;
+            idata.data[ind + 1] = update;
+            idata.data[ind + 2] = update;
+        }
+
+    return idata;
+}
+
+
+var selectedEffect = "horizontal";
 
 var canvas = document.createElement('canvas');
 jQuery('#result').append(canvas);
@@ -82,28 +137,75 @@ $("#result-open").off().on('click', function () {
 });
 
 function previewFile(file) {
-    //let reader = new FileReader();
+    dropArea.classList.remove('active');
     loadImage(file, function (img) {
-        //canvas = img;
-        //var img = new Image();
-        //img.onload = function () {
-            var width = 1024;
-            var height = width * img.height / img.width;
-            canvas.width = width;
-            canvas.height = height;
-            var ctx = canvas.getContext('2d');
+        var width = 1024;
+        var height = width * img.height / img.width;
+        canvas.width = width;
+        canvas.height = height;
+        var ctx = canvas.getContext('2d');
 
-            ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, width, height);
-            var idata = ctx.getImageData(0, 0, width, height);
+        ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, width, height);
+        var idata = ctx.getImageData(0, 0, width, height);
 
-            idata = processIII(idata, width, height);
+        switch (selectedEffect) {
+            case 'horizontal':
+                idata = processIII(idata, width, height);
+                break;
+            case 'vertical':
+                idata = process___(idata, width, height);
+                break;
+            case 'zigzag':
+                idata = processZZZ(idata, width, height);
+                break;
+        }
 
-            ctx.putImageData(idata, 0, 0);
+        ctx.putImageData(idata, 0, 0);
 
-            jQuery('#result').addClass('_visible');
-            jQuery('a').attr('href', canvas.toDataURL('image/png'));
-        //};
-        //img.src = reader.result;
+        jQuery('#result').addClass('_visible');
+        jQuery('a').attr('href', canvas.toDataURL('image/png'));
+
         jQuery('#fileElem').val(null);
     }, {orientation: 1})
 }
+
+function updateDescription() {
+    var text = '';
+    switch (selectedEffect) {
+        case 'horizontal':
+            text = 'Horizontal Lines Illusion';
+            break;
+        case 'vertical':
+            text = 'Vertical Lines Illusion';
+            break;
+        case 'zigzag':
+            text = 'Zigzag Lines Illusion';
+            break;
+    }
+    $('.effect-description').html(text);
+}
+
+updateDescription();
+
+function updateSelection(modifier) {
+    $('.effects-selector__item').removeClass('_selected');
+    $('.effects-selector__item.' + modifier).addClass('_selected');
+}
+
+$('.effects-selector__item._horizontal').off().on('click', function () {
+    updateSelection('_horizontal');
+    selectedEffect = "horizontal";
+    updateDescription();
+});
+
+$('.effects-selector__item._vertical').off().on('click', function () {
+    updateSelection('_vertical');
+    selectedEffect = "vertical";
+    updateDescription();
+});
+
+$('.effects-selector__item._zigzag').off().on('click', function () {
+    updateSelection('_zigzag');
+    selectedEffect = "zigzag";
+    updateDescription();
+});
